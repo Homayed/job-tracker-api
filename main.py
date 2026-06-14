1ddb06d7d93126facf845eac6db878a313155977
Homayed <naser154439@gmail.com>
2026-06-15 00:46:53 +0100

    Refactor application note endpoints into router

--- main.py
+++ main.py
@@ -9,6 +9,35 @@ app.include_router(companies.router)
 app.include_router(applications.router)
 app.include_router(interviews.router)
 app.include_router(notes.router)
+from fastapi import FastAPI
+from database import Base, engine
+from routers import auth_routes, users, companies,applications,interviews,notes
+
+app = FastAPI()
+app.include_router(auth_routes.router)
+app.include_router(users.router)
+app.include_router(companies.router)
+app.include_router(applications.router)
+app.include_router(interviews.router)
+app.include_router(notes.router)
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Job Tracker API is running"
+    }
+
+
+
+
+
+
+
+
+
 
 Base.metadata.create_all(bind=engine)
 
